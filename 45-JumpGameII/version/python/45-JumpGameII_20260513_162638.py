b1d46871fa4e606d524c1a3eec2a80734f8afe53
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-13 16:26:38 +1000

    Create 45-JumpGameII

---
+++ 45-JumpGameII/version/python/45-JumpGameII_20260513_162638.py
@@ -0,0 +1,15 @@
+# Last updated: 13/05/2026, 16:26:38
+1class Solution:
+2    def jump(self, nums: List[int]) -> int:
+3        jumps = 0
+4        reach = 0
+5        currentEnd = 0
+6
+7        for i in range(len(nums) - 1):
+8            reach = max(reach, i + nums[i])
+9
+10            if i == currentEnd:
+11                jumps += 1
+12                currentEnd = reach
+13        
+14        return jumps
