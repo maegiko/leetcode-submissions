1bf420bf30dfc0871b987a2272068116d045a33c
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-25 17:35:59 +1000

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/version/java/35-SearchInsertPosition_20260525_173558.java
@@ -0,0 +1,21 @@
+// Last updated: 25/05/2026, 17:35:58
+1class Solution {
+2    public int searchInsert(int[] nums, int target) {
+3        int left = 0;
+4        int right = nums.length - 1;
+5
+6        while (left <= right) {
+7            int middle = (left + right) / 2;
+8
+9            if (target > nums[middle]) {
+10                left = middle + 1;
+11            } else if (target < nums[middle]) {
+12                right = middle - 1;
+13            } else {
+14                return middle;
+15            }
+16        }
+17
+18        return left;
+19    }
+20}
