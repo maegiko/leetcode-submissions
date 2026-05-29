11aa37030c0cfd0811f6227de04460ea5718dea2
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-29 15:47:48 +1000

    Create 202-HappyNumber

---
+++ 202-HappyNumber/version/java/202-HappyNumber_20260529_154746.java
@@ -0,0 +1,25 @@
+// Last updated: 29/05/2026, 15:47:46
+1class Solution {
+2    public boolean isHappy(int n) {
+3        Set<Integer> seen = new HashSet<>();
+4
+5        while (n != 1) {
+6            int sum = 0;
+7
+8            while (n > 0) {
+9                int ones = n % 10;
+10                sum += ones * ones;
+11                n = n / 10;
+12            }
+13
+14            if (seen.contains(sum)) {
+15                return false;
+16            } else {
+17                seen.add(sum);
+18                n = sum;
+19            }
+20        }
+21
+22        return true;
+23    }
+24}
