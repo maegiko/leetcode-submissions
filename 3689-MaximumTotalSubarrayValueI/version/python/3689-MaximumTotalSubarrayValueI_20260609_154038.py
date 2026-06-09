da0daeae34704a1ce7c2889abeac2213ce6e33a7
Kenneth Koon <kennethk.dev@gmail.com>
2026-06-09 15:40:38 +1000

    Create 3689-MaximumTotalSubarrayValueI

---
+++ 3689-MaximumTotalSubarrayValueI/version/python/3689-MaximumTotalSubarrayValueI_20260609_154038.py
@@ -0,0 +1,13 @@
+# Last updated: 09/06/2026, 15:40:38
+1class Solution:
+2    def maxTotalValue(self, nums: List[int], k: int) -> int:
+3        maxVal = 0
+4        minVal = float("inf")
+5
+6        for num in nums:
+7            maxVal = max(maxVal, num)
+8            minVal = min(minVal, num)
+9        
+10        diff = maxVal - minVal
+11
+12        return diff * k
