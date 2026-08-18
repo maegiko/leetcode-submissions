8f7afe0ef4985ef263d94a2a9c88aab74a898fb2
Kenneth Koon <kennethk.dev@gmail.com>
2026-08-18 14:56:04 +1000

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,21 @@
+// Last updated: 18/08/2026, 14:56:03
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (target > nums[middle]) {
+                left = middle + 1;
+            } else if (target < nums[middle]) {
+                right = middle - 1;
+            } else {
+                return middle;
+            }
+        }
+
+        return -1;
+    }
+}
